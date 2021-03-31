00d92dfb954ee02bc7531064ec3e95f2d6d9c3dc
Sergey Prika <prikasp@yandex-team.ru>
2021-03-31 23:19:11 +0300

    Set cookie

--- src/main/kotlin/ru/fellowgames/kt_backend/SpyGameController.kt
+++ src/main/kotlin/ru/fellowgames/kt_backend/SpyGameController.kt
@@ -1,9 +1,13 @@
 package ru.fellowgames.kt_backend
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.WebUtils
 
 @Controller
@@ -16,9 +20,15 @@ class SpyGameController {
   }
 
   @GetMapping("/spy_game")
-  fun game(model: Model): String {
-    println(model)
-    model["title"] = "Шпион"
+  fun game(model: Model,
+      @RequestParam(value = "game_id") game_id: String,
+      @CookieValue(value = "game_id_c") game_id_c: String?,
+      response: HttpServletResponse): String {
+    model["title"] = game_id + (game_id_c ?: "")
+    
+    val cookie = Cookie("game_id_c", game_id)
+    response.addCookie(cookie)
+
     return "spy_game"
   }
 
